d58566c0b7ff7f78896ce0fd432ec1f95ea02532
tugtsc <tugtsc>
2025-07-03 14:26:33 +0700

    commit

---
+++ IdentityAuthen/WebApplication1/Data/Migrations/ApplicationDb/20250703065801_AddCountry.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Authen.Data.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class AddCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryFlag",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                schema: "Identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CountryFlag",
+                schema: "Identity",
+                table: "AspNetUsers");
+        }
+    }
+}
